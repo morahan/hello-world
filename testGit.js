e3ef09f2b5202cd3d991e53e0c91da36260ada9c
Michael Morahan <mmorahan@Michaels-MacBook-Pro-4.local>
2019-11-27 15:42:10 -0700

    my second commit from terminal

---
+++ testGit.js
@@ -0,0 +1,85 @@
+// let number = 7;
+// let checkNum = function(num){
+//   if (num > 5) {
+//     console.log("You Know It")
+//   } else {
+//     return false;
+//   }
+// }
+// checkNum(number);
+//
+
+
+// Musician
+// Song
+// Samples
+
+// let musician = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   songs: ['',''],
+//   songIDs: [0, 0],
+//   id: null
+// };
+//
+// let newMusician = function(first, second, songArr, songIdArr) {
+//   musician.firstName = first;
+//   musician.lastName = second;
+//   musician.songs = songArr;
+//   musician.songIDs = songIdArr;
+// }
+//
+//
+// let joeSongs = ["song1", "song2", "song3"];
+// let joeSongIDs = [1,2,3]
+// newMusician("joe", "smith", joeSongs, joeSongIDs )
+
+//  ---- using constructor function to create template profile for musicians
+
+// function Musician (firstName, lastName, songs, songIds){
+//   this.firstName = firstName;
+//   let self = this;
+//   this.lastName = lastName;
+//   this.songs = songs;
+//   this.songIds = [];
+//   let songIdFunc = function() {
+//     for (let x = 1; x < self.songs.length + 1; x++) {
+//       self.songIds.push(x + 100);
+//     }
+//   };
+//   songIdFunc();
+// };
+//
+// let mike = new Musician("Mike", "Morahan", ["Walking On Mars", "Minor Ting"]);
+
+console.log(mike)
+
+
+
+
+
+// New musician.id = 0011;
+//
+// console.log(musician);
+
+// let song = {
+//   title: "first Song",
+//   samples: 0,
+//   original: true,
+//   cover: false,
+//   musician: 001
+// };
+//
+//
+// let sample = {
+//
+// };
+
+
+// let ownershipPercentage = function(){
+//   musician
+// }
+
+
+
+let var newMessage = "this is something new";
